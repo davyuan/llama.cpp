065fb309b7c5b1fe17589667fea41bc7f5d1889f
David Yuan <davyuan@gmail.com>
2026-01-31 10:53:18 -0600

    update

--- ggml/src/ggml.c
+++ ggml/src/ggml.c
@@ -12651,7 +12651,7 @@ static void ggml_compute_forward_mul_mat(
     // nb01 >= nb00 - src0 is not transposed
     //   compute by src0 rows
 #if defined(GGML_BITNET_ARM_TL1)
-    if (src0->type == GGML_TYPE_F16 && src1->type == GGML_TYPE_F32) {
+    /*if (src0->type == GGML_TYPE_F16 && src1->type == GGML_TYPE_F32) {
         const int64_t r2 = ne12/ne02;
         const int64_t r3 = ne13/ne03;
 
@@ -12719,7 +12719,7 @@ UseGgmlVec_Dot_TL1:
             ggml_barrier(params->threadpool);
             return;
         }
-    }
+    }*/
 
     if (src0->type == GGML_TYPE_TL1) {
         if(!ggml_bitnet_can_mul_mat(src0, src1, dst) || sizeof(bitnet_float_type) != 4 ||
